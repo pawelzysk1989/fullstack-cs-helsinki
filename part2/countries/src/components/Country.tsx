47c2a85224ccb2775b31c9902b31e61e7771d22b
pawelzysk <pawelzysk1989@gmail.com>
2021-11-04 00:01:36 +0100

    weather

--- part2/countries/src/components/Country.tsx
+++ part2/countries/src/components/Country.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Country as CountryType } from '../models/Country';
+import Weather from './Weather';
 
 type Props = {
   country: CountryType;
@@ -17,15 +18,18 @@ const Country = ({ country }: Props) => {
         population: <strong>{country.population}</strong>
       </div>
       <div>
-        <h3>languages</h3>
+        <h3>Spoken languages</h3>
         <ul>
           {Object.values(country.languages).map((lang) => (
             <li key={lang}>{lang}</li>
           ))}
         </ul>
       </div>
-
       <img src={country.flags.png} alt="country flag" />
+
+      {country.capital.map((location) => (
+        <Weather key={location} location={location} />
+      ))}
     </div>
   );
 };
---
+++ part2/countries/src/components/Weather.tsx
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+import { Weather as WeatherType } from '../models/Weather';
+import WeatherDetails from './WeatherDetails';
+
+const access_key = import.meta.env['VITE_WEATHER_API_KEY'];
+
+type Props = {
+  location: string;
+};
+
+const Weather = ({ location }: Props) => {
+  const [weatherInfo, setWeatherInfo] = useState<WeatherType | undefined>(undefined);
+
+  useEffect(() => {
+    axios
+      .get<WeatherType>('http://api.weatherstack.com/current', {
+        params: {
+          access_key,
+          query: location,
+        },
+      })
+      .then((response) => {
+        setWeatherInfo(response.data);
+      });
+  }, [location]);
+
+  return (
+    <div>
+      <h3>Weather in {location}</h3>
+      {weatherInfo && <WeatherDetails weather={weatherInfo} />}
+    </div>
+  );
+};
+
+export default Weather;
